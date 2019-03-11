cb974bc33b08b009a200b1de086636e46a846f87
F80 <43803343+f80dev@users.noreply.github.com>
2019-03-11 12:12:31 +0100

    Update suitesrc_setup.sh

--- crm/suitesrc_setup.sh
+++ crm/suitesrc_setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 rm -r -f /opt/bitnami
+mkdir /opt/bitnami
 mkdir /opt/bitnami/mariadb_data
 mkdir /opt/bitnami/suitecrm_data
+
+mkdir /etc/nginx
+mkdir /etc/nginx/certs
+mkdir /etc/nginx/vhost.d
+mkdir /usr/share/nginx
+mkdir /usr/share/nginx/html
 
 #voir https://techoverflow.net/2018/12/15/solving-bitnam-docker-redmine-cannot-create-directory-bitnami-mariadb-permission-denied/
 sudo chown -R 1001:1001 /opt/bitnami/mariadb_data
@@ -10,8 +17,7 @@ sudo chown -R 1001:1001 /opt/bitnami/suitecrm_data
 
 #Installation de suitecrm
 cd /root
-rm -f docker-compose.yml
-wget https://raw.githubusercontent.com/f80dev/myRousseauAutoServer/master/crm/docker-compose.yml
+rm -f docker-compose.yml && wget https://raw.githubusercontent.com/f80dev/myRousseauAutoServer/master/crm/docker-compose.yml
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
