40bc34b04643b59d0e5456860fd57710a2518894
F80 <43803343+f80dev@users.noreply.github.com>
2019-03-11 09:44:25 +0100

    Update suitesrc_setup.sh

--- crm/suitesrc_setup.sh
+++ crm/suitesrc_setup.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
+rm -r -f /opt/bitnami
 mkdir /opt/bitnami/mariadb_data
 mkdir /opt/bitnami/suitecrm_data
 
-#voir https://techoverflow.net/2018/12/15/solving-bitnami-docker-redmine-cannot-create-directory-bitnami-mariadb-permission-denied/
+#voir https://techoverflow.net/2018/12/15/solving-bitnam-docker-redmine-cannot-create-directory-bitnami-mariadb-permission-denied/
 sudo chown -R 1001:1001 /opt/bitnami/mariadb_data
 sudo chown -R 1001:1001 /opt/bitnami/suitecrm_data
 
+#Installation de suitecrm
+cd /root
+rm -f docker-compose.yml
 wget https://raw.githubusercontent.com/f80dev/myRousseauAutoServer/master/crm/docker-compose.yml
-docker-compose up -d
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+docker-compose up
+
+
+#fabrication des certificats
+cd /opt/bitnami/suitecrm_data/apache/conf/bitnami/certs
+certbot certonly --standalone -d server.f80.fr -d www.server.f80.fr
+openssl x509 -outform der -in /etc/letsencrypt/live/server.f80.fr/cert.pem -out server.crt
+openssl rsa -outform der  -in /etc/letsencrypt/live/server.f80.fr/privkey.pem -out server.key
+
+reboot
 
 #ouvrir le browser sur http://<adresse du serveur>/index.php?module=Home&action=index
